659bc58cb345a0c8a7064bd350b1ec2f86e24ebe
Pontus Sköldström <pontus.skoldstrom@ericsson.com>
2021-10-18 11:03:35 +0200

    updated

--- config/swedish_keys.h
+++ config/swedish_keys.h
@@ -5,36 +5,38 @@
 #include <dt-bindings/zmk/keys.h>
 
 // some swedish key codes
-
-#define SE_LT NON_US_BSLH  // <
-#define SE_GT LS(SE_LT)    // >
-#define SE_PIPE RA(SE_LT)  // |
-#define SE_AMPS LS(N6)     // &
-#define SE_LBKT RA(N8)     // [
-#define SE_RBKT RA(N9)     // ]
-#define SE_LBRC RA(N7)     // {
-#define SE_RBRC RA(N0)     // }
-#define SE_LPAR LS(N8)     // (
-#define SE_RPAR LS(N9)     // )
-#define SE_FLSH LS(N7)     // /
-#define SE_BLSH RA(MINUS)  //
-#define SE_AT RA(N2)       // @
-#define SE_DLLR RA(N4)     // $
-#define SE_EURO RA(N5)     // €
-#define SE_DQT LS(N2)      // "
-#define SE_SQT BSLH        // '
-#define SE_TILDE RA(RBKT)  // ~
-#define SE_STAR PIPE       // *
-#define SE_MU RA(M)        // µ
 #define SE_ACNT EQUAL      // ´
-#define SE_CARET RBRC      // ^
-#define SE_EQUAL LS(N0)    // =
-#define SE_PLUS MINUS      // +
-#define SE_SEMI LS(COMMA)  // ;
-#define SE_AT RA(N2)       // @
-#define SE_QMARK LS(MINUS) // ?
-#define SE_AO LBKT         // Å
 #define SE_AE SQT          // Ä
-#define SE_OE SEMI         // Ö
+#define SE_AMPS LS(N6)     // &
+#define SE_AO LBKT         // Å
+#define SE_AT RA(N2)       // @
+#define SE_BLSH RA(MINUS)  //
+#define SE_CARET RBRC      // ^
+#define SE_CLN LS(DOT)     // :
+#define SE_DLLR RA(N4)     // $
+#define SE_DQT LS(N2)      // "
+#define SE_EQUAL LS(N0)    // =
+#define SE_EURO RA(N5)     // €
+#define SE_EXCL LS(N1)     // !
+#define SE_FLSH LS(N7)     // /
+#define SE_GT LS(SE_LT)    // >
+#define SE_HASH LS(N3)     // #
+#define SE_LBKT RA(N8)     // [
+#define SE_LBRC RA(N7)     // {
+#define SE_LPAR LS(N8)     // (
+#define SE_LT NON_US_BSLH  // <
 #define SE_MINUS FSLH      // -
+#define SE_MU RA(M)        // µ
+#define SE_OE SEMI         // Ö
+#define SE_PIPE RA(SE_LT)  // |
+#define SE_PLUS MINUS      // +
+#define SE_PRCNT LS(N5)    // %
+#define SE_QMARK LS(MINUS) // ?
+#define SE_RBKT RA(N9)     // ]
+#define SE_RBRC RA(N0)     // }
+#define SE_RPAR LS(N9)     // )
+#define SE_SEMI LS(COMMA)  // ;
+#define SE_SQT BSLH        // '
+#define SE_STAR PIPE       // *
+#define SE_TILDE RA(RBKT)  // ~
 #define SE_UNDS LS(FSLH)   // _
